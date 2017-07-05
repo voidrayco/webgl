639928ffc2f6f7fda6c99a8df6ee2bde068f3324
Samuel Dillow <tarwich@gmail.com>
2017-07-05 17:03:26 -0500

    Implemented tslint

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
 import './index.html';
-console.log('This... is the index.')
--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,14 @@ const {resolve} = require('path');
 const tslintLoader = {loader: 'tslint-loader', options: {
   fix: true,
   emitErrors: true,
+  // failOnHint: true,
 }};
 
 module.exports = {
   entry: './src',
   module: {
     rules: [
-      {test: /\.tsx?/, use: tslintLoader},
+      {test: /\.tsx?/, use: tslintLoader, enforce: 'pre'},
       {test: /\.tsx?/, use: {loader: 'ts-loader', options: {transpileOnly: true}}},
       {test: /index.html$/, use: {loader: 'file-loader', options: {name: 'index.html'}}},
     ],
