780c4f11f940b19b7a83153d638f359755983f2f
Samuel Dillow <tarwich@gmail.com>
2017-07-05 15:13:21 -0500

    WIP: Setup tslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const {resolve} = require('path');
 
+const tslintLoader = {loader: 'tslint-loader', options: {
+  fix: true,
+  emitErrors: true,
+}};
+
 module.exports = {
   entry: './src',
   module: {
     rules: [
+      {test: /\.tsx?/, use: tslintLoader},
       {test: /\.tsx?/, use: {loader: 'ts-loader', options: {transpileOnly: true}}},
       {test: /index.html$/, use: {loader: 'file-loader', options: {name: 'index.html'}}},
     ],
