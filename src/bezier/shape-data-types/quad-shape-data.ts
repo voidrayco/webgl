2bc19309b0f1a730f312b11ef44593b84cd2401f
Yue Yu <heryo_yo@163.com>
2017-07-17 10:12:22 -0500

    Yo Yo updated
    I created curve.ts ,curve-shape ts, interpolation.ts
    I modified main.tsx,quad-shape.ts, quad-shape-data,ts bezier-gl.tsx, quad-generator.ts

--- src/bezier/shape-data-types/quad-shape-data.ts
+++ src/bezier/shape-data-types/quad-shape-data.ts
@@ -1,12 +1,15 @@
 import { IPoint } from 'webgl-surface/primitives/point';
-import { ISize } from 'webgl-surface/primitives/size';
+import { RGBColor } from 'd3-color';
 
 /**
+ * YoYo changed here
  * This defines any data we want associated with quads within this rendering
  * ecosystem
  */
 export interface IQuadShapeData {
+  color:RGBColor,
   id: number,
-  position: IPoint,
-  size: ISize,
+  lineWidth:number,
+  p1: IPoint,
+  p2: IPoint,
 }
