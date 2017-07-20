63f940b8b7ec68bf8374fb37dbfd57a3d50b30f9
Diniden <diniden@outlook.com>
2017-07-20 10:34:26 -0600

    Cache and selection stubs in place

--- src/chord-chart/generators/chord/chord-base-cache.ts
+++ src/chord-chart/generators/chord/chord-base-cache.ts
@@ -4,8 +4,6 @@ import { CurveType } from 'webgl-surface/primitives/curved-line';
 import { ShapeBufferCache } from 'webgl-surface/util/shape-buffer-cache';
 import { ICurvedLineData } from '../../shape-data-types/curved-line-data';
 
-const debug = require('debug')('chord-chart');
-
 /**
  * Responsible for generating the static chords in the system
  *
@@ -19,8 +17,9 @@ export class ChordBaseCache extends ShapeBufferCache<CurvedLineShape<ICurvedLine
   }
 
   buildCache() {
+
     this.buffer = [
-      new CurvedLineShape(CurveType.Circular, {x: 0, y: 0}, {x: 300, y: 0}, [/*{x: 100, y: 400}*/], rgb(1, 0, 0, 1), 40),
+      new CurvedLineShape(CurveType.Bezier, {x: 0, y: 0}, {x: 300, y: 0}, [/*{x: 100, y: 400}*/], rgb(1, 0, 0, 1), 40),
     ];
 
     this.buffer[0].lineWidth = 10;
