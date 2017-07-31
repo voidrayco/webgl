71ecc548af354dff2c2d8a0577c789dfea6f1657
Jonathan Haar <johnny.tomcat@gmail.com>
2017-07-31 10:38:47 -0500

    CAT-22 enable autofix

--- src/chord-chart/index.tsx
+++ src/chord-chart/index.tsx
@@ -7,7 +7,7 @@ import { IChordChartConfig } from './generators/types';
 import { ChordChartGL } from './gl/chord-chart-gl';
 import { Selection, SelectionType } from './selections/selection';
 
-const debug = require('debug')('chord-index');
+// DEBUG const debug = require('debug')('chord-index');
 const testChordData = require('./test-data/chord-data.json');
 
 interface IChordChartProps {
@@ -67,8 +67,8 @@ export class ChordChart extends React.Component<IChordChartProps, IChordChartSta
 
     if (selections.length > 0) {
       let selection;
-      // If has outer ring thing grab it instead 
-      const filteredSelections = selections.filter(s => s.type === 1)
+      // If has outer ring thing grab it instead
+      const filteredSelections = selections.filter(s => s.type === 1);
       if (filteredSelections.length > 0) {
         selection = filteredSelections.reduce((prev, current) => (current.distanceTo(world) < prev.distanceTo(world)) ? current : prev);
       } else {
