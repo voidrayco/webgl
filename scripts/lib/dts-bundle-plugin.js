88fca41cbc38b8c240e36dfbad05e45acdb3a6b5
Diniden <diniden@outlook.com>
2017-08-17 15:43:30 -0600

    Included removal of excess declaration files after bundling process

--- scripts/lib/dts-bundle-plugin.js
+++ scripts/lib/dts-bundle-plugin.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs-extra');
 
 /**
  * This is a simple plugin wrapper to execute the dts bundler. This was
@@ -25,7 +26,23 @@ DtsBundlePlugin.prototype.apply = function(compiler) {
       outputAsModuleFolder: true,
     });
 
-    callback();
+    // Before doing a major removal of files, make a timeout so the system
+    // doesn't hang in limbo for eternity
+    const timeoutId = setTimeout(() => {
+      console.error('ERROR: Deleting type definition source files took too long!');
+      callback();
+    }, 30000);
+
+    // Attempt the removal of the input source files
+    fs.remove(input, err => {
+      if (err) {
+        console.error('ERROR: Could NOT delete type definition source files!');
+        console.error(err.stack || err.message);
+      }
+
+      clearTimeout(timeoutId);
+      callback();
+    });
   });
 };
 
