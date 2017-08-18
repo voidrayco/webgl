f8d3801c9e485059d141dd3c07f2fa8b9a5a9a17
Diniden <diniden@outlook.com>
2017-08-18 09:14:45 -0600

    Clean ups

--- scripts/lib/dts-bundle-plugin.js
+++ scripts/lib/dts-bundle-plugin.js
@@ -36,12 +36,23 @@ DtsBundlePlugin.prototype.apply = function(compiler) {
     });
 
     // Clear out any roots that may have been imported
+    // NOTE: A timeout is used as there is not a teribly reliable way to determining
+    // when the type bundler has finished it's task
     setTimeout(() => {
       let file = fs.readFileSync(out, 'utf8');
       const files = fs.readdirSync(input);
+
+      // Our system uses roots in the webpack configuration making statements
+      // like 'webgl-surface/blah/blah' possible. Our bundler blindly adds all
+      // items in our src in our bundle, but it gets confused about roots and
+      // has no way to specify roots for it. Thus it thinks it's an external and
+      // attempts to import them. Since the class is included already from the
+      // blind inclusion, we need to just eliminate the imports to our internal
+      // root modules, but leave the imports for the third party lib types.
       files.forEach(filename => {
         file = file.replace(new RegExp(`import.+${filename}.+`, 'g'), '');
       });
+
       fs.writeFileSync(out, file, 'utf8');
       callback();
     }, 2000);
