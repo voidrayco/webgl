d2358dd9879456ec1ddbe09c41ffb8c5e249421b
Jacob Myers <jacob1234098@gmail.com>
2017-09-22 21:16:05 +0100

    setting default image for both errors. returning false as these cases are not drawn.

--- src/webgl-surface/drawing/texture/atlas-manager.ts
+++ src/webgl-surface/drawing/texture/atlas-manager.ts
@@ -245,6 +245,7 @@ export class AtlasManager {
       else {
         // Log an error
         console.error(`Could not fit image into atlas ${image.imagePath}`);
+        image = this.setDefaultImage(image, atlasName);
         return false;
 
       }
@@ -254,7 +255,7 @@ export class AtlasManager {
       // Log an error and load a default image
       console.error(`Could not load image ${image.imagePath}`);
       image = this.setDefaultImage(image, atlasName);
-      return true;
+      return false;
     }
   }
 
