7983dc021974e38d50783ba0641a25ed3f8d62e4
Yue Yu <heryo_yo@163.com>
2017-10-19 14:09:51 -0500

    label line to the end

--- src/webgl-surface/drawing/texture/atlas-manager.ts
+++ src/webgl-surface/drawing/texture/atlas-manager.ts
@@ -438,7 +438,8 @@ export class AtlasManager {
         // Set the dimensions of the canvas/texture space we will be using to rasterize
         // The label. Use the label's rasterization controls to aid in rendering the label
         canvas.width = labelSize.width + texture.label.rasterizationPadding.width;
-        canvas.height = labelSize.height + texture.label.rasterizationPadding.height;
+        canvas.height = labelSize.height + texture.label.rasterizationPadding.height - 10;
+        canvas.style.backgroundColor = 'rgba(158,45,38,1)';
 
         if (ctx) {
           const fontSize = label.fontSize;
