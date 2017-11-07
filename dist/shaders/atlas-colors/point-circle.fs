4751fb622ee6007b981b7459eb67e41326c0759b
Diniden <diniden@outlook.com>
2017-11-07 08:23:25 -0700

    Version 0.5.1

--- dist/shaders/atlas-colors/point-circle.fs
+++ dist/shaders/atlas-colors/point-circle.fs
@@ -1,10 +1,11 @@
 varying vec4 vertexColor;
+varying float edgeSharpness;
 
 float circle(vec2 coord, float radius){
   vec2 dist = coord - vec2(0.5);
 
 	return 1.0 - smoothstep(
-    radius - (radius * 0.01),
+    radius - (radius * edgeSharpness),
     radius + (radius * 0.01),
     dot(dist, dist) * 4.0
   );
