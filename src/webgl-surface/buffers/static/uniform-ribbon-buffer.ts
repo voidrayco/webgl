1e7aa0fc56b59086fc6c2c49aa5ecbe1224fc273
heryoyo1989 <heryoyo1989@gmail.com>
2017-12-11 12:21:16 -0600

    add class and method comment to ribbonShape

--- src/webgl-surface/buffers/static/uniform-ribbon-buffer.ts
+++ src/webgl-surface/buffers/static/uniform-ribbon-buffer.ts
@@ -6,8 +6,7 @@ import { AtlasManager } from '../../drawing/texture/atlas-manager';
 import { AttributeSize, BufferUtil, UniformAttributeSize } from '../../util/buffer-util';
 import { BaseBuffer } from '../base-buffer';
 
-const debug = require('debug')('webgl:uniform-ribbon-buffer');
-
+// CONSTANT
 const MAX_SEGMENTS_PER_CURVE = 100;
 const VERTICES_PER_SEGMENT = 6;
 const VERTICES_PER_CURVE = VERTICES_PER_SEGMENT * MAX_SEGMENTS_PER_CURVE;
@@ -20,7 +19,6 @@ export class UniformRibbonBuffer extends BaseBuffer <RibbonShape<any> | RibbonSh
 
   init(material: ShaderMaterial, unitCount: number, shared?: UniformRibbonBuffer) {
     this.bufferItems = BufferUtil.makeBufferItems();
-    debug('init ribbon~');
     // Declare the structure of the uniform data in the instanceData uniform
     this.bufferItems.uniformAttributes = [
       {
@@ -186,7 +184,6 @@ export class UniformRibbonBuffer extends BaseBuffer <RibbonShape<any> | RibbonSh
     if (buffer.length > 0 && atlasManager) {
       const colorRef: ReferenceColor = buffer[0].startColor;
       const colorBase = colorRef.base;
-
       const material: ShaderMaterial = this.bufferItems.system.material as ShaderMaterial;
       const uniforms: { [k: string]: IUniform} = material.uniforms;
       const atlas = atlasManager.getAtlasTexture(colorBase.atlasReferenceID);
--- src/webgl-surface/drawing/shape/ribbon-shape.ts
+++ src/webgl-surface/drawing/shape/ribbon-shape.ts
@@ -19,6 +19,16 @@ export interface IRibbonOptions extends ICurvedLineOptions{
   controlPoints: IPoint[];
 }
 
+/**
+ * This defines a drawable ribbon shape.  It contains the information necessary
+ * to efficiently render the ribbon.
+ *
+ * @export
+ * @class RibbonShape
+ * @extends {CurvedLineShape<T>}
+ * @template T An associated data type that can be linked to this shape to make data retrieval from interacting
+ *             with the shape easier
+ */
 export class RibbonShape<T> extends CurvedLineShape<T> {
   /** When true, this line will cache the segments of the curve as rendered quads */
   cachesQuadSegments: boolean;
@@ -30,15 +40,22 @@ export class RibbonShape<T> extends CurvedLineShape<T> {
   endColor: ReferenceColor;
   /** The starting color components of this line. This must be a color reference */
   startColor: ReferenceColor;
-  /** The first bezier line of ribbon */
+  /** The starting point of ribbon's second bezier line */
   start2: IPoint;
-
+  /** The ending point of ribbon's second bezier line */
   end2: IPoint;
-
+  /** Then center of hemiSphere where two starting points are located */
   center1: IPoint;
-
+  /** Then center of hemiSphere where two ending points are located */
   center2: IPoint;
 
+  /**
+   *
+   * Creates and instance of RibbonShape
+   *
+   * @param {IRibbonOptions} options  The options for creating this ribbon
+   *
+   */
   constructor(options: IRibbonOptions) {
     super(options);
 
@@ -51,6 +68,11 @@ export class RibbonShape<T> extends CurvedLineShape<T> {
     this.center2 = options.center2;
   }
 
+  /**
+   * Returns a new instance of this object that mimicks the properties of this Object
+   *
+   * @returns {RibbonShape<T>} The cloned object
+   */
   clone() {
     // Perform the clone
     const clone: RibbonShape<T> = new RibbonShape<T>({
@@ -72,11 +94,20 @@ export class RibbonShape<T> extends CurvedLineShape<T> {
     return clone;
   }
 
+  /**
+   * @override
+   * Return distance from a point to ribbon. If the point is in ribbon, return 0
+   * Otherwise use method of superclass.
+   */
   distanceTo(point: IPoint): number {
     if (this.pointInside(point)) return 0;
     return super.distanceTo(point);
   }
 
+  /**
+   * Test whether a point is in the ribbon using Point Inclusion in Polygon Test method
+   * @param point
+   */
   pointInside(point: IPoint): boolean {
     const points = this.getRibbonStrip();
     const nvert: number = points.length;
@@ -97,13 +128,22 @@ export class RibbonShape<T> extends CurvedLineShape<T> {
     return c;
   }
 
+  /**
+   * This prodeuces all the points along the two bezier lines.
+   * The order of all the points pushed should be clockwise.
+   *
+   * @return {IPoint[]} The points to create the quads that make each segment of the ribbon.
+   *
+   */
   getRibbonStrip(): IPoint[]{
     const strip: IPoint[] = [];
     const dt = 1 / this.resolution;
     const c1 = this.controlPoints[0];
+
     for (let i = 0 ; i <= this.resolution; i++ ) {
       strip.push(bezier2(dt * i, this.start, this.end, c1));
     }
+
     for (let i = this.resolution; i >= 0; i--){
       strip.push(bezier2(dt * i, this.start2, this.end2, c1));
     }
